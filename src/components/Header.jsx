b02de8cb87ee8739af78c8c9424cadc28c01261e
davi <davijluna@gmail.com>
2022-06-20 17:17:29 -0300

    9 implemente os elementos do reader na tela principal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,9 @@ function Header() {
   return (
     <div>
       <h2>Header</h2>
+      <p data-testids="profile-top-btn">Ok</p>
+      <p data-testids="page-title">Ok</p>
+      <p data-testids="search-top-btn">Ok</p>
     </div>
   );
 }
