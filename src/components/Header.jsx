9c8c94a076aa61091b31c7bd446ffde0ec44bb3c
davi <davijluna@gmail.com>
2022-06-20 19:29:26 -0300

    adição de switch e react useState

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 
-function Header() {
-  return (
-    <div>
-      <h2>Header</h2>
-      <button
-        type="button"
-        data-testids="profile-top-btn"
-        onClick="#"
-      >
-        Ok
+function Header(props) {
+  const { title } = props;
+  const [redirect, setRedirect] = useState('');
+  const [buttonVisible, setbuttonVisible] = useState(true);
 
-      </button>
-      <title data-testids="page-title">Ok</title>
-      <button
-        type="button"
-        data-testid="search-input"
-        onClick="#"
-      >
-        Busca
+  switch (redirect) {
+  case 'profile':
+    return <Redirect to="/profile" />;
 
-      </button>
-    </div>
-  );
+  default:
+    return (
+      <div>
+        <button
+          type="button"
+          data-testids="profile-top-btn"
+          onClick={ () => setRedirect('profile') }
+        >
+          Ok
+
+        </button>
+        <title data-testids="page-title">{title}</title>
+        <button
+          type="button"
+          data-testid="search-input"
+          onClick={ () => setbuttonVisible(!buttonVisible) }
+        >
+          Busca
+
+        </button>
+      </div>
+    );
+  }
 }
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default Header;
