1abdc3d3467fa2f4b2fd02a7a52f0061552e4e52
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-06-20 18:10:59 -0300

    Correção do redirects do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
@@ -6,35 +6,46 @@ import mealIcon from '../images/mealIcon.svg';
 import './Footer.css';
 
 function Footer() {
-  return (
-    <footer data-testid="footer">
-      <h2>Footer</h2>
-      <button
-        type="button"
-        data-testid="drinks-bottom-btn"
-        className="footer-button-icon"
-        onClick={ () => <Redirect to="/drinks" /> }
-      >
-        { drinkIcon }
-      </button>
-      <button
-        type="button"
-        data-testid="explore-bottom-btn"
-        className="footer-button-icon"
-        onClick={ () => <Redirect to="/explore" /> }
-      >
-        { exploreIcon }
-      </button>
-      <button
-        type="button"
-        data-testid="food-bottom-btn"
-        className="footer-button-icon"
-        onClick={ () => <Redirect to="/foods" /> }
-      >
-        { mealIcon }
-      </button>
-    </footer>
-  );
+  const [redirect, setRedirect] = useState(null);
+
+  switch (redirect) {
+  case 'drinks':
+    return <Redirect to="/drinks" />;
+  case 'explore':
+    return <Redirect to="/explore" />;
+  case 'foods':
+    return <Redirect to="/foods" />;
+  default:
+    return (
+      <footer data-testid="footer">
+        <h2>Footer</h2>
+        <button
+          type="button"
+          data-testid="drinks-bottom-btn"
+          className="footer-button-icon"
+          onClick={ () => setRedirect('drinks') }
+        >
+          { drinkIcon }
+        </button>
+        <button
+          type="button"
+          data-testid="explore-bottom-btn"
+          className="footer-button-icon"
+          onClick={ () => setRedirect('explore') }
+        >
+          { exploreIcon }
+        </button>
+        <button
+          type="button"
+          data-testid="food-bottom-btn"
+          className="footer-button-icon"
+          onClick={ () => setRedirect('foods') }
+        >
+          { mealIcon }
+        </button>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
---
+++ src/test/Footer.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+
+import App from '../App';
+
+describe('Teste do Footer', () => {
+  it('avalia a renderização dos elementos do Footer', () => {
+    render(<App />);
+  });
+});
