beb9b484d7ea01f82ce4e8655e43b8cc9a156762
Leonan Costa <leonanfecosta@gmail.com>
2022-06-20 19:40:42 -0300

    implementa a verificação de estado e exibe um alerta caso a API nao retorne nada

---
+++ src/components/InputRadio.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function InputRadio({ dataTestid, id, name, value, onClick, labelContent }) {
+  return (
+    <label htmlFor={ id }>
+      <input
+        type="radio"
+        data-testid={ dataTestid }
+        id={ id }
+        name={ name }
+        value={ value }
+        onClick={ onClick }
+      />
+      {labelContent}
+    </label>
+  );
+}
+
+InputRadio.propTypes = {
+  dataTestid: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  labelContent: PropTypes.string.isRequired,
+};
+
+export default InputRadio;
