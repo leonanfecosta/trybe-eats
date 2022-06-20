23012c0acc73b7189c42e3a8aeae459e825b650d
Mariana Fernanda <ariana.fernanda@icloud.com>
2022-06-20 17:01:11 -0300

    Implementa os elementos da tela de bebida e comida

--- src/pages/explore/ExploreDrinks.jsx
+++ src/pages/explore/ExploreDrinks.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function ExploreDrinks() {
   return (
     <div>
-      <h2>ExploreDrinks</h2>
+      <h2>Explore Drinks</h2>
+      <Link to="/explore/drinks/ingredients">
+        <button type="button" data-testid="explore-by-ingredient">By Ingredient</button>
+      </Link>
+      <Link to="/">
+        <button type="button" data-testid="explore-surprise">Surprise me!</button>
+      </Link>
     </div>
   );
 }
+// Adicionar a rota para o 'Surprise me!'
 
 export default ExploreDrinks;
--- src/pages/explore/ExploreFoods.jsx
+++ src/pages/explore/ExploreFoods.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function ExploreFoods() {
   return (
     <div>
-      <h2>ExploreFoods</h2>
+      <h2>Explore Foods</h2>
+      <Link to="/explore/foods/ingredients">
+        <button type="button" data-testid="explore-by-ingredient">By Ingredient</button>
+      </Link>
+      <Link to="/explore/foods/nationalities">
+        <button type="button" data-testid="explore-by-nationality">By Nationality</button>
+      </Link>
+      <Link to="/">
+        <button type="button" data-testid="explore-surprise">Surprise me!</button>
+      </Link>
     </div>
   );
 }
+// Adicionar a rota para o 'Surprise me!'
 
 export default ExploreFoods;
