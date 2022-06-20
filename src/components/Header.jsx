75bdbf3ad9e2c2f62407e18abf96b59542d316cf
davi <davijluna@gmail.com>
2022-06-20 18:07:13 -0300

    button e title do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,23 @@ function Header() {
   return (
     <div>
       <h2>Header</h2>
-      <p data-testids="profile-top-btn">Ok</p>
-      <p data-testids="page-title">Ok</p>
-      <p data-testids="search-top-btn">Ok</p>
+      <button
+        type="button"
+        data-testids="profile-top-btn"
+        onClick="#"
+      >
+        Ok
+
+      </button>
+      <title data-testids="page-title">Ok</title>
+      <button
+        type="button"
+        data-testid="search-input"
+        onClick="#"
+      >
+        Busca
+
+      </button>
     </div>
   );
 }
