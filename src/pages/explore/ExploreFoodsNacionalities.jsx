aa8808ff5022687ad827d5eb7277e4f309d3b980
davi <davijluna@gmail.com>
2022-06-27 18:37:14 -0300

    feat: Implementa select na tela de nacionalidades

--- src/pages/explore/ExploreFoodsNacionalities.jsx
+++ src/pages/explore/ExploreFoodsNacionalities.jsx
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import styles from '../../styles/ExploreFoodsNacionalities.module.css';
+import getNacionalities from '../../services/nacionalities';
 
 function ExploreFoodsNacionalities() {
+  const [nationalities, setNationalities] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // const NUMBER = 12;
+    getNacionalities().then((item) => setNationalities(item));
+  }, []);
+
+  useEffect(() => {
+    setLoading(false);
+  }, [nationalities]);
+  console.log(nationalities);
+
   return (
     <div className={ styles.exploreFoodsNacionalities }>
       <Header title="Explore Nationalities" showButton route="null" />
       <Footer />
+      {loading && (
+        <h4
+          style={ { textAlign: 'center', marginTop: '100px' } }
+        >
+          Loading...
+        </h4>
+      )}
+      {
+        !loading && (
+          <select data-testid="explore-by-nationality-dropdown">
+            {nationalities.map((nacionality) => (
+              <option
+                key={ nacionality.strArea }
+                data-testid={ `${nacionality.strArea}-option` }
+              >
+                {nacionality.strArea}
+
+              </option>
+            ))}
+          </select>
+        )
+      }
+
     </div>
   );
 }
