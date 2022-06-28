1aa27f8d571bb45d2f745530ca077fdcaa0f48b8
davi <davijluna@gmail.com>
2022-06-28 20:46:59 -0300

    feat: Implementa tela de explorar nacionalidade

--- src/pages/explore/ExploreFoodsNacionalities.jsx
+++ src/pages/explore/ExploreFoodsNacionalities.jsx
@@ -3,20 +3,36 @@ import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import styles from '../../styles/ExploreFoodsNacionalities.module.css';
 import getNacionalities from '../../services/nacionalities';
+import getMealsByArea from '../../services/getMealsByArea';
+import CardFood from '../../components/cardFood';
+import { getMeals } from '../../services/mealApi';
 
 function ExploreFoodsNacionalities() {
   const [nationalities, setNationalities] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [select, setSelect] = useState('All');
+  const [mealsArea, setMealsArea] = useState([]);
+  const NUMBER = 12;
+
+  const updateMeals = async () => {
+    setMealsArea(await getMeals());
+  };
 
   useEffect(() => {
-    // const NUMBER = 12;
     getNacionalities().then((item) => setNationalities(item));
   }, []);
 
   useEffect(() => {
     setLoading(false);
-  }, [nationalities]);
-  console.log(nationalities);
+  }, [nationalities, mealsArea]);
+
+  useEffect(() => {
+    if (select === 'All') {
+      updateMeals();
+    } else {
+      getMealsByArea(select).then((item) => setMealsArea(item));
+    }
+  }, [select]);
 
   return (
     <div className={ styles.exploreFoodsNacionalities }>
@@ -31,9 +47,19 @@ function ExploreFoodsNacionalities() {
       )}
       {
         !loading && (
-          <select data-testid="explore-by-nationality-dropdown">
+          <select
+            data-testid="explore-by-nationality-dropdown"
+            onChange={ (event) => setSelect(event.target.value) }
+          >
+            <option
+              name="nacionality"
+              data-testid="All-option"
+            >
+              All
+            </option>
             {nationalities.map((nacionality) => (
               <option
+                name="nacionality"
                 key={ nacionality.strArea }
                 data-testid={ `${nacionality.strArea}-option` }
               >
@@ -44,7 +70,19 @@ function ExploreFoodsNacionalities() {
           </select>
         )
       }
-
+      {(!loading && mealsArea) && mealsArea.slice(0, NUMBER).map((food, index) => (
+        <CardFood
+          key={ food.idMeal }
+          id={ food.idMeal }
+          srcImg={ food.strMealThumb }
+          alt={ food.strMeal }
+          name={ food.strMeal }
+          dataTestIdCard={ `${index}-recipe-card` }
+          dataTestIdImg={ `${index}-card-img` }
+          dataTestIdName={ `${index}-card-name` }
+          route="food"
+        />
+      ))}
     </div>
   );
 }
