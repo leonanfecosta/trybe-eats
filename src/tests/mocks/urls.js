2c229626602c482e62c9a6ebd8e5ec63c5386ed8
Leonan Costa <leonanfecosta@gmail.com>
2022-06-29 15:54:33 -0300

    feat: começa a implementação dos testes de Food

--- src/tests/mocks/urls.js
+++ src/tests/mocks/urls.js
@@ -6,6 +6,11 @@ import mealsById from './mealsById';
 import drinksById from './drinksById';
 import areas from './areas';
 import mealsByArea from './mealsByArea';
+import mealsByCategory from './mealsByCategory';
+import mealsByIngredient from './mealsByIngredient';
+import mealsByName from './mealsByName';
+import mealsByFirstLetter from './mealsByFirstLetter';
+import mealsByFirstLetterRedirect from './mealsByFirstLetterRedirect';
 
 const URLS = {
   'https://www.themealdb.com/api/json/v1/1/search.php?s=': meals,
@@ -18,6 +23,11 @@ const URLS = {
   'https://www.thecocktaildb.com/api/json/v1/1/random.php': drinksById,
   'https://www.themealdb.com/api/json/v1/1/list.php?a=list': areas,
   'https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian': mealsByArea,
+  'https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef': mealsByCategory,
+  'https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken': mealsByIngredient,
+  'https://www.themealdb.com/api/json/v1/1/search.php?s=chocolate': mealsByName,
+  'https://www.themealdb.com/api/json/v1/1/search.php?f=w': mealsByFirstLetter,
+  'https://www.themealdb.com/api/json/v1/1/search.php?f=y': mealsByFirstLetterRedirect,
 };
 
 export default URLS;
