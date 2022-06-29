c2b50f0e193a5f74881d0754f51315036f074083
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-06-29 09:20:29 -0300

    feat: cria o objeto contendo as urls para mock e mock das areas

---
+++ src/tests/mocks/urls.js
@@ -0,0 +1,19 @@
+import meals from './meals';
+import drinks from './drinks';
+import mealsCategories from './mealsCategories';
+import drinksCategories from './drinksCategories';
+import mealsById from './mealsById';
+import drinksById from './drinksById';
+import areas from './areas';
+
+const URLS = {
+  'https://www.themealdb.com/api/json/v1/1/search.php?s=': meals,
+  'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=': drinks,
+  'https://www.themealdb.com/api/json/v1/1/list.php?c=list': mealsCategories,
+  'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list': drinksCategories,
+  'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52977': mealsById,
+  'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=15997': drinksById,
+  'https://www.themealdb.com/api/json/v1/1/list.php?a=list': areas,
+};
+
+export default URLS;
