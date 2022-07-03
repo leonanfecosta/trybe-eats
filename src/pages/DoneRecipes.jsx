d80ba80e2bf3d5b6d1fb57cd2afe1d03ff88f561
Leonan Costa <leonanfecosta@gmail.com>
2022-07-03 18:52:02 -0300

    fix: bug da tela DoneRecipes na hora de compartilhar

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import copy from 'clipboard-copy';
-import { Link } from 'react-router-dom';
 import Buttons from '../components/Buttons';
 import Header from '../components/Header';
-import shareIcon from '../images/shareIcon.svg';
 import styles from '../styles/DoneRecipes.module.css';
+import DoneRecipesDetails from '../components/DoneRecipesDetails';
 
 function DoneRecipes() {
   const [completedRecipes, setCompletedRecipes] = useState([]);
-  const [wasCopied, setWasCopied] = useState(false);
 
   useEffect(() => {
     const recipes = JSON.parse(localStorage.getItem('doneRecipes')) || [];
@@ -56,79 +53,19 @@ function DoneRecipes() {
       </nav>
       <section>
         {(completedRecipes || []).map((recipe, index) => (
-          <div key={ recipe.id } className={ styles.doneRecipesCards }>
-            <div className={ styles.parte1 }>
-              <Link to={ `${recipe.type}s/${recipe.id}` }>
-                <img
-                  src={ recipe.image }
-                  alt={ recipe.name }
-                  data-testid={ `${index}-horizontal-image` }
-                />
-              </Link>
-              <aside>
-                {recipe.type === 'food' && (recipe.tags || []).map((tag) => (
-                  <p
-                    key={ tag }
-                    data-testid={ `${index}-${tag}-horizontal-tag` }
-                    className={ styles.tags }
-                  >
-                    {tag}
-                  </p>
-                ))}
-              </aside>
-            </div>
-            <div className={ styles.parte2 }>
-              <Link to={ `${recipe.type}s/${recipe.id}` }>
-                <h4
-                  data-testid={ `${index}-horizontal-name` }
-                >
-                  {recipe.name}
-                </h4>
-              </Link>
-
-              {recipe.type === 'food' && (
-                <p
-                  data-testid={ `${index}-horizontal-top-text` }
-                >
-                  {`${recipe.nationality} - ${recipe.category}`}
-                </p>
-              )}
-
-              {recipe.type === 'drink' && (
-                <p
-                  data-testid={ `${index}-horizontal-top-text` }
-                >
-                  {recipe.alcoholicOrNot}
-                </p>
-              )}
-
-              <p data-testid={ `${index}-horizontal-done-date` }>
-                {recipe.doneDate}
-              </p>
-
-              <input
-                type="image"
-                data-testid={ `${index}-horizontal-share-btn` }
-                onClick={ () => {
-                  copy(`http://localhost:3000/${recipe.type}s/${recipe.id}`);
-                  setWasCopied(true);
-                } }
-                src={ shareIcon }
-                alt="shareIcon"
-                style={ { width: '15px' } }
-              />
-              {wasCopied && (
-                <p
-                  data-testid={ `${index}-horizontal-share-btn` }
-                  style={ { fontSize: '12px' } }
-                >
-                  Link copied!
-                </p>
-              )}
-
-            </div>
-
-          </div>
+          <DoneRecipesDetails
+            key={ recipe.id }
+            id={ recipe.id }
+            type={ recipe.type }
+            name={ recipe.name }
+            index={ index }
+            image={ recipe.image }
+            tags={ recipe.tags }
+            nationality={ recipe.nationality }
+            category={ recipe.category }
+            alcoholicOrNot={ recipe.alcoholicOrNot }
+            doneDate={ recipe.doneDate }
+          />
         ))}
       </section>
     </div>
